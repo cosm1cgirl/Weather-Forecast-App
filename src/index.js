4563c64272a6f81e3de8e88163cfbe21e8189ec2
cosm1cgirl <166348211+cosm1cgirl@users.noreply.github.com>
2024-04-21 20:10:24 +0200

    Updated index.js
    -Function to update city name and fetch
     weather data on form submission.
    -Event listener for form submission to
     update city and fetch weather data

---
+++ src/index.js
@@ -0,0 +1,11 @@
+function updateCity(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
+}
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", updateCity);
+
+searchCity("Cape Town");
