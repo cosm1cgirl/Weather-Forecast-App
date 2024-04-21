d412a4f69a0584261894673e2e6c4eca387c68d9
cosm1cgirl <166348211+cosm1cgirl@users.noreply.github.com>
2024-04-21 20:38:58 +0200

    Updated index.js
    Function to update current temperature added

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
+function updateTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  temperatureElement.innerHTML = Math.round(temperature) + "°c";
+}
+
 function searchCity(city) {
   let apiKey = "efc7a995b0ta50f31c388oe39854d44b";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
